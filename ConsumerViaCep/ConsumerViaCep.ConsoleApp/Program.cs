4a6db6a20f7fadf01bc000302f74bdaa387b9910
Paiva <danpaiva@live.com>
2026-01-19 20:26:10 -0300

    feat: add codigo na Program.cs

--- ConsumerViaCep/ConsumerViaCep.ConsoleApp/Program.cs
+++ ConsumerViaCep/ConsumerViaCep.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsumerViaCep.Domain.Interfaces;
+using ConsumerViaCep.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddSingleton<ICepService, CepService>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var cepService = serviceProvider.GetService<ICepService>();
+var dados = await cepService.GetAddressByCepAsync("01001000");
+
+Console.WriteLine($"Endereço: {dados.Logradouro}, {dados.Bairro} - {dados.Localidade}/{dados.Uf}");
