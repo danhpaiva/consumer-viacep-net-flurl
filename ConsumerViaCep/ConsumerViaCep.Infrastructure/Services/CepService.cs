19cfb2c7561889204bfb59cf07d3786350b56e67
Paiva <danpaiva@live.com>
2026-01-19 19:57:15 -0300

    feat: criar Servico para buscar dados

---
+++ ConsumerViaCep/ConsumerViaCep.Infrastructure/Services/CepService.cs
@@ -0,0 +1,21 @@
+﻿using ConsumerViaCep.Domain.Interfaces;
+using ConsumerViaCep.Domain.Models;
+using System.ComponentModel.Design;
+
+namespace ConsumerViaCep.Infrastructure.Services;
+
+public class CepService : ICepService
+{
+    private const string BaseUrl = "https://viacep.com.br/ws";
+
+    public async Task<ViaCepResponse> GetAddressByCepAsync(string cep)
+    {
+        if (string.IsNullOrWhiteSpace(cep))
+            throw new ArgumentException("CEP não pode ser vazio.");
+
+        return await BaseUrl
+            .AppendPathSegment(cep)
+            .AppendPathSegment("json")
+            .GetJsonAsync<ViaCepResponse>();
+    }
+}
